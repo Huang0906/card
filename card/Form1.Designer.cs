046be69f1208881b0b818cef22213b32c1462a1f
振益 黃 <huang050906@gmail.com>
2024-05-27 07:13:06 +0800

    1

--- card/Form1.Designer.cs
+++ card/Form1.Designer.cs
@@ -28,27 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(310, 295);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(140, 160);
+            this.textBox1.Location = new System.Drawing.Point(147, 148);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(463, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.Size = new System.Drawing.Size(520, 23);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -56,7 +44,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -65,14 +52,8 @@
 
         }
 
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
         #endregion
 
-        private Button button1;
         private TextBox textBox1;
     }
 }
--- card/Form1.cs
+++ card/Form1.cs
@@ -8,18 +8,28 @@ using System.Net.Sockets;
 using System.Globalization;
 using System.Net.Mail;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using System.Net.Http;
+using System.Data;
+using System.IO;
+using System.ComponentModel.Design.Serialization;
+
+/*
+作者：黃振益
+想試看看的話可以執行並參考#39~42的數值
+想傳到自己的line的話,在這裡申請的api https://notify-bot.line.me/zh_TW/ 並貼到#105 or #121就可以了
+*/
+
+
 namespace card
 {
     public partial class Form1 : Form
     {
-
-        private string a;
-        private string ab = "123";
-
+        private HttpClient httpClient;
         public Form1()
         {
             InitializeComponent();
+            textBox1.KeyDown += TextBox1_KeyDown;
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
         }
 
         private async void Form1_Load(object sender, EventArgs e)
@@ -27,55 +37,47 @@ namespace card
             await Task.Run(() => UpdateNtpTime());
         }
 
+        Dictionary<string, string> idMappings = new Dictionary<string, string>
+        {
+            { "1962338564", "蔡雅鈞  座號:32"},
+            { "3528143108", "吳惟閎  座號:03"},
+            { "3502290436", "洪凱綸  座號:11"},
+            { "1961852420", "黃振益  座號:23"}
+        };
         private void UpdateNtpTime()
         {
-            try
+            string ntpServer = "pool.ntp.org";
+            using (var ntpClient = new UdpClient(ntpServer, 123))
             {
-                string ntpServer = "pool.ntp.org";
-                using (var ntpClient = new UdpClient(ntpServer, 123))
+                var ntpData = new byte[48];
+                ntpData[0] = 0x1B;
+                ntpClient.Send(ntpData, ntpData.Length);
+                var endPoint = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0);
+                List<byte> responseDataList = new List<byte>();
+                int receivedBytes;
+                do
                 {
-                    var ntpData = new byte[48];
-                    ntpData[0] = 0x1B;
-                    ntpClient.Send(ntpData, ntpData.Length);
-                    var endPoint = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0);
-                    List<byte> responseDataList = new List<byte>();
-                    int receivedBytes;
-                    do
-                    {
-                        byte[] receivedData = ntpClient.Receive(ref endPoint);
-                        receivedBytes = receivedData.Length;
-                        responseDataList.AddRange(receivedData);
-                    } while (receivedBytes == 48);
-                    byte[] responseData = responseDataList.ToArray();
-                    if (responseData.Length < 48)
-                    {
-                        throw new Exception("無效的 NTP 回應");
-                    }
-                    ulong intPart = BitConverter.ToUInt32(responseData, 43);
-                    ulong fractPart = BitConverter.ToUInt32(responseData, 47);
-                    ulong milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
-                    DateTime ntpTime = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(milliseconds);
+                    byte[] receivedData = ntpClient.Receive(ref endPoint);
+                    receivedBytes = receivedData.Length;
+                    responseDataList.AddRange(receivedData);
+                } while (receivedBytes == 48);
+                byte[] responseData = responseDataList.ToArray();
+                if (responseData.Length < 48)
+                {
+                    throw new Exception("無效的 NTP 回應");
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("例外情況: " + ex.ToString());
-                MessageBox.Show("取得 NTP 時間時發生錯誤: " + ex.Message);
+                ulong intPart = BitConverter.ToUInt32(responseData, 43);
+                ulong fractPart = BitConverter.ToUInt32(responseData, 47);
+                ulong milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+                DateTime ntpTime = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(milliseconds);
             }
         }
 
-        private async Task<string> GetNtpTimeString()
+        public async Task<string> GetNtpTimeString()
         {
-            try
-            {
-                DateTime ntpTime = await FetchNtpTimeAsync();
-                return ntpTime.ToString("yyyy-MM-dd HH:mm:ss");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("獲取 NTP 時間時發生錯誤: " + ex.Message);
-                throw;
-            }
+            DateTime ntpTime = await FetchNtpTimeAsync();
+            string ntptime = ("yyyy-MM-dd HH:mm:ss");
+            return ntpTime.ToString();
         }
 
         private async Task<DateTime> FetchNtpTimeAsync()
@@ -87,49 +89,69 @@ namespace card
             return localTime;
         }
 
-
-        private async void textBox1_TextChanged(object sender, EventArgs e)
+        public async void TextBox1_KeyDown(object sender, KeyEventArgs e)
         {
 
-        }
-
-
-
-        private async void button1_Click(object sender, EventArgs e)
-        {
-
-            try
+            if (textBox1.Text.Length == 10 && e.KeyCode == Keys.Enter)
             {
-                string ntpTime = await GetNtpTimeString();
-                string a = "xxx", b = "xxx";
-                string abc = "現在時間\n";
-                abc += ntpTime + "\n" + "名字: " + a + "座號: " + b + "已繳交";
-                using (HttpClient httpClient = new HttpClient())
+                DateTime now = DateTime.Now;
+                DateTime latetime = new DateTime(now.Year, now.Month, now.Day, 10, 0, 0);
+                string id = textBox1.Text;
+                string mappedText = idMappings.ContainsKey(id) ? idMappings[id] : "未知";
+                if (mappedText == "未知")
                 {
-                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
-                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "vEdBisbXcROwnZkBbcRtMS4ZUqTQX9bIW0SQ2VllbZd");
+                    string ntpTime = await GetNtpTimeString();
+                    string abc = "現在時間\n";
+                    abc += ntpTime + "\n" + $"名字: {mappedText}";
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "vEdBisbXcROwnZkBbcRtMS4ZUqTQX9bIW0SQ2VllbZd");//notify vEdBisbXcROwnZkBbcRtMS4ZUqTQX9bIW0SQ2VllbZd
                     var content = new Dictionary<string, string>
-                {
-                    { "message", abc },
-                };
-
+                    {
+                        { "message", abc },
+                    };
                     using (var response = await httpClient.PostAsync("https://notify-api.line.me/api/notify", new FormUrlEncodedContent(content)))
                     {
                         response.EnsureSuccessStatusCode();
                     }
+                    textBox1.Clear();
+                }
+                else if (now > latetime)
+                {
+                    string ntpTime = await GetNtpTimeString();
+                    string abc = "現在時間\n";
+                    abc += ntpTime + "\n" + $"名字: {mappedText}\n超過{latetime}\n已遲到";
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "vEdBisbXcROwnZkBbcRtMS4ZUqTQX9bIW0SQ2VllbZd");//專題群組 LPAr3fUyRDx3k4j7QkCptTE5K80TocuesPsDR0OxWur
+                    var content = new Dictionary<string, string>
+                    {
+                        { "message", abc },
+                    };
+                    using (var response = await httpClient.PostAsync("https://notify-api.line.me/api/notify", new FormUrlEncodedContent(content)))
+                    {
+                        response.EnsureSuccessStatusCode();
+                    }
+                    textBox1.Clear();
+                }
+                else
+                {
+                    string ntpTime = await GetNtpTimeString();
+                    string abc = "現在時間\n";
+                    abc += ntpTime + "\n" + $"名字: {mappedText}\n未超過{latetime}\n打卡成功";
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "vEdBisbXcROwnZkBbcRtMS4ZUqTQX9bIW0SQ2VllbZd");
+                    var content = new Dictionary<string, string>
+                    {
+                        { "message", abc },
+                    };
+                    using (var response = await httpClient.PostAsync("https://notify-api.line.me/api/notify", new FormUrlEncodedContent(content)))
+                    {
+                        response.EnsureSuccessStatusCode();
+                    }
+                    textBox1.Clear();
                 }
             }
-            catch (HttpRequestException ex)
+            else if (textBox1.Text.Length < 10 && e.KeyCode == Keys.Enter)
             {
-                var statusCode = (int)ex.StatusCode;
-                var errorMessage = ex.Message;
-                Console.WriteLine($"API 錯誤: 狀態碼 {statusCode}, 錯誤信息: {errorMessage}");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("發生錯誤: " + ex.Message);
+                textBox1.Clear();
+                MessageBox.Show("未知的值");
             }
         }
-
     }
 }
